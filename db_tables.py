a3e6e2d83e938cb72a7baa1a5777901af396d5f4
Zhuyi Xue (Mac OS) <zhuyi.xue@mail.utoronto.ca>
2012-06-13 08:40:57 -0400

    second commit

--- db_tables.py
+++ db_tables.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import time
+
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, Float
 
@@ -8,5 +10,18 @@ Base = declarative_base()
 class YoungNode(Base):
     __tablename__ = "youngnode"
     
-    id = Column(Integer, primary_key=True)
-    
+    ID = Column(Integer, primary_key=True)
+    hostname = Column(String)
+    start_time = Column(Float)
+    ip_address = Column(String)
+
+    def __init__(self, hostname, start_time, ip_address):
+        self.hostname = hostname
+        self.start_time = start_time
+        self.ip_address = ip_address
+
+    def __repr__(self):
+        return "<YoungNode {0} started at {1} ({2})>".format(
+            self.hostname, 
+            time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime(self.start_time)),
+            self.ip_address)
--- obj.py
+++ obj.py
@@ -3,8 +3,13 @@
 import os
 import time
 import socket
+import subprocess
 
+import flask
+import requests
 import sqlalchemy
+
+import db_tables as dbtb
 
 DEBUG=True
 
@@ -17,6 +22,7 @@ class Node(object):
         self.pbs_o_workdir = os.getenv('PBS_O_WORKDIR', 'zyxue_O_WORKDIR')
         self.pbs_momport = os.getenv('PBS_MOMPORT', 'zyxue_MOMPORT')
         self.pbs_environment = os.getenv('PBS_ENVIRONMENT', 'zyxue_ENVIRONMENT') # e.g. PBS_INTERACTIVE
+        self.hostname = os.getenv('HOSTNAME', 'zyxue_HOSTNAME')
         self.ip_address = socket.gethostbyname(socket.gethostname())
         self.port = '999'                                  # can be configured
         
@@ -27,29 +33,62 @@ class Node(object):
 class Master(Node):
     def init_db(self, db):
         engine = sqlalchemy.create_engine('sqlite:///{0}'.format(db), echo=DEBUG)
-        return engine
-
-    def connect_db(self, db):
-        engine = self.init_db(db)
         sessionmaker = sqlalchemy.orm.sessionmaker(bind=engine, autoflush=True, autocommit=False)
         return sessionmaker
 
-    def introduce_myself(self):
-        
+    def connect_db(self, db):
+        sessionmaker = self.init_db(db)                  # init db
+        return sessionmaker()                            # create a new session
+
+    def say_i_am_young(self):
+        session = self.connect_db()
+        ynode = dbtb.YoungNode(hostname=self.hostname,
+                               start_time=self.start_time,
+                               ip_address=self.ip_address)
+        self.write_db(session, ynode)
+        session.commit()
         return self.estimated_end_time
 
-    def read_db(self):
+    def query_db(self, sqlcmd):
+        # probably query_db needs to be more specific for individual case
         pass
 
     def write_db(self, session, item_row):
-        session.write(item_row)
+        # only write a single item to the db
+        session.add(item_row)
 
-    def broadcase_url(self):
-        # write ip:port to a __HOSTNAME__.txt
-        pass
+    def broadcase_url(self, hostfile="__HOSTNAME__.txt"):
+        """write 'ip:port' to a __HOSTNAME__.txt"""
+        if not os.path.exists(hostfile):
+            with open(hostfile, 'w') as opf:
+                opf.write("http://{0}:{1}\n".format(self.ip_address, self.port))
+
+    def be_in_charge(self):
+        app = flask.Flask(__name__)
+        app.config.from_object(__name__)
+        app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+
+        # self.active = True
+        
+        @app.before_request
+        def before_request():
+            # Just store whatever you want on flask.g
+            flask.g.session = self.connect_db()
+
+        @app.teardown_request
+        def teardown_request(exception):
+            if hasattr(flask.g, 'session'):
+                flask.g.session.commit()
+
+        @app.route('/get_cmd', method=["GET"])
+        def dispatch_cmd():
+            # what should I do here? probably consult the conf file, how to
+            # construct the mpirun command?
+            pass
 
     def arouse_slaves(self):
         # arouse the number of slaves according to the configuration files
+        # loop through the dirs specified in the conf file and qsub run.py
         pass
 
     def dispatch_cmd(self):
@@ -80,15 +119,26 @@ class Slave(Node):
     database) other than run the command received from Master, and report to
     the master when finished, then such cycle repeated until it dies
     """
+    def get_url(self, hostfile="__HOSTNAME__.txt"):
+        with open(hostfile) as f:
+            url = f.readline().strip()
+        return url
+
     def connect_master(self):
+        # I don't think this function is useful
         pass
 
     def get_cmd(self):
-        pass
+        url = self.get_url()
+        r = requests.get("{0}/get_cmd".format(url))
+        return r.text                                       # needed to be tested
 
     def run_cmd(self):
-        pass
+        cmd = self.get_cmd()
+        returncode = subprocess.call(cmd)
+        return returncode
 
-    def repot(self):
+    def report(self):
+        # using post method?
         """report a list of files with absolute paths"""
         pass
