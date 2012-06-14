147ed8f31500f7be551786063d70bb78e5a06f88
Zhuyi Xue (Mac OS) <zhuyi.xue@mail.utoronto.ca>
2012-06-13 23:18:22 -0400

    finished get_the_youngest_ip

--- obj.py
+++ obj.py
@@ -6,9 +6,11 @@ import socket
 import logging
 import subprocess
 
+import yaml
 import flask
 import requests
 import sqlalchemy
+from sqlalchemy import orm
 
 import db_tables as dbtb
 
@@ -25,6 +27,23 @@ class Node(object):
         self.pbs_environment = os.getenv('PBS_ENVIRONMENT', 'zyxue_ENVIRONMENT') # e.g. PBS_INTERACTIVE
         self.hostname = os.getenv('HOSTNAME', 'zyxue_HOSTNAME')
         self.bigmaster_flag = os.getenv('BIGMASTER_FLAG')
+
+        try:
+            self.yamlf = os.environ["YAML"]
+        except KeyError:
+            print "YAML env not found, serious problem!"        
+
+        rootdir = os.path.dirname(os.path.abspath(self.yamlf))
+        params = yaml.load(open(self.yamlf))
+        self.db = os.path.join(rootdir, params.get('database'))
+        self.topology = os.path.join(rootdir, params.get('topology'))
+        self.hostfile = os.path.join(rootdir, params.get('hostfile'))
+
+        # sysname = params['sysname']
+        # rootdir = params['rootdir']
+        # misc_params = params['MISC']
+        # system = System(sysname, rootdir, misc_params)
+
         self.ip_address = socket.gethostbyname(socket.gethostname())
         self.port = '999'                                  # can be configured
         
@@ -33,43 +52,32 @@ class Node(object):
         self.estimated_end_time = self.start_time + self.walltime
 
 class Master(Node):
-    def init_db(self, db):
-        engine = sqlalchemy.create_engine('sqlite:///{0}'.format(db), echo=DEBUG)
-        sessionmaker = sqlalchemy.orm.sessionmaker(bind=engine, autoflush=True, autocommit=False)
-        return sessionmaker
+    def init_db(self):
+        engine = sqlalchemy.create_engine('sqlite:///{0}'.format(self.db), echo=DEBUG)
+        dbtb.YoungNode.metadata.create_all(engine)          # CREATE TABLE
+        S = orm.sessionmaker(bind=engine, autoflush=True, autocommit=False)
+        return S
 
-    def connect_db(self, db):
-        sessionmaker = self.init_db(db)                  # init db
-        return sessionmaker()                            # create a new session
+    def connect_db(self):
+        S = self.init_db()                    # init db
+        return S()                            # create a new session
 
-    def write_db(self, session, item_row):
-        session.add(item_row)
-
-    def say_i_am_young(self, db):
-        session = self.connect_db(db)
-        ynode = dbtb.YoungNode(hostname=self.hostname,
-                               start_time=self.start_time,
-                               ip_address=self.ip_address)
-        self.write_db(session, ynode)
-        session.commit()
-        return self.estimated_end_time
-
-    def broadcast_url(self, hostfile="__HOSTNAME__.txt"):
-        """write 'ip:port' to a __HOSTNAME__.txt"""
-        if not os.path.exists(hostfile):
-            with open(hostfile, 'w') as opf:
+    def broadcast_url(self):
+        """write 'ip:port' to a hostfile"""
+        if not os.path.exists(self.hostfile):
+            with open(self.hostfile, 'w') as opf:
                 opf.write("http://{0}:{1}\n".format(self.ip_address, self.port))
 
-    def get_the_youngest_url(self):
-        # session = self.connect_db(db)
-        # session.query(YoungNode).
-        pass
+    def get_the_youngest_ip(self):
+        session = self.connect_db()
+        item = session.query(dbtb.YoungNode).order_by("start_time desc").first()
+        return item.ip_address
 
     def invite_switch(self):
-        url = self.get_the_youngest_url()
-        r = requests.post("http://{0}/invite_switch".format(url))
-        if r.text == url:
-            logging.info('bigmaster switched from {0} to {1}'.format(self.ip_address, url))
+        ip = self.get_the_youngest_ip()
+        r = requests.post("http://{0}:{1}/invite_switch".format(ip), self.port)
+        if r.text == ip:
+            logging.info('bigmaster switched from {0} to {1}'.format(self.ip_address, ip))
 
     def wakeup(self):
         app = flask.Flask(__name__)
@@ -84,35 +92,44 @@ class Master(Node):
             if hasattr(flask.g, 'session'):
                 flask.g.session.commit()
 
+        @app.route('/imyoung', methods=["POST"])
+        def imyoung():
+            session = self.connect_db()
+            ynode = dbtb.YoungNode(hostname=flask.request.form['hostname'],
+                                   start_time=flask.request.form['start_time'],
+                                   ip_address=flask.request.form['ip_address'])
+            session.add(ynode)
+            session.commit()
+            return self.estimated_end_time
+
         @app.route('/get_cmd', methods=["GET"])
         def dispatch_cmd():
+            return "date >> lala; pwd >> lala; sleep 10"
             # what should I do here? probably consult the conf file, how to
             # construct the mpirun command?
-            pass
 
         @app.route('/report', methods=["POST"])
-        def receive_report():
+        def reply_to_report():
             # Process the report (basically a list of files with absolute paths
             # from the slaves), do some necessary db IO, exchange should happen
             # here.
-            pass
+            return "report received"
 
         @app.route('/request_switch', methods=["GET"])
-        def reply_to_switch(self):
+        def reply_to_request(self):
             """This message must come from the slave which is on the same node"""
             self.invite_switch()
             return "done"
 
         @app.route('/invite_switch', methods=["GET"])
-        def reply_to_switch(self):
+        def reply_to_invitation(self):
             """
             This request must come from the big master on another node,
             i.e. I am not the big master when receiving this request
             """ 
             # maybe the new url should be broadcasted by the old server? not sure
-            self.broadcast_url(hostfile="__HOSTNAME__.txt")
+            self.broadcast_url()
             return self.ip_address
-
         app.run()
 
 class Slave(Node):
@@ -121,13 +138,20 @@ class Slave(Node):
     database) other than run the command received from Master, and report to
     the master when finished, then such cycle repeated until it dies
     """
-    def get_url(self, hostfile):
-        with open(hostfile) as f:
+    def say_i_am_young(self):
+        url = self.get_url(self.hostfile)
+        params = {'hostname': self.hostname,
+                  'start_time': self.start_time,
+                  'ip_address': self.ip_address}
+        requests.post("{0}/imyoung".format(url), data=params)
+
+    def get_url(self):
+        with open(self.hostfile) as f:
             url = f.readline().strip()
         return url
 
-    def get_cmd(self, hostfile):
-        url = self.get_url(hostfile)
+    def get_cmd(self):
+        url = self.get_url(self.hostfile)
         r = requests.get("{0}/get_cmd".format(url))
         return r.text                                       # needed to be tested
 
@@ -149,29 +173,29 @@ class Slave(Node):
                 "log": "lala.log",
                 }
 
-    def request_switch(self, hostfile):
+    def request_switch(self):
         """
         request the big master, which should be on the same node with this
         slave to invite another node for switch
         """
-        url = self.get_url(hostfile)
+        url = self.get_url(self.hostfile)
         r = requests.get("{0}/request_switch".format(url))
         return r.text
 
-class System(object):
-    def __init__(self, sysname, rootdir, params):
-        self.rootdir = rootdir                              # required
-        self.sysname = sysname                              # required
+# class System(object):
+#     def __init__(self, sysname, rootdir, params):
+#         self.rootdir = rootdir                              # required
+#         self.sysname = sysname                              # required
 
-        fp = self.fullpath
-        _top = params['topology']
-        self.topf = fp(_top) if _top else fp('{0}.top'.format(self.sysname))
-        _db  = params['database']
-        self.dbf  = fp(_db) if _db else fp('{0}.db'.format(self.sysname))
+#         fp = self.fullpath
+#         _top = params['topology']
+#         self.topf = fp(_top) if _top else fp('{0}.top'.format(self.sysname))
+#         _db  = params['database']
+#         self.dbf  = fp(_db) if _db else fp('{0}.db'.format(self.sysname))
 
-    def fullpath(self, f):
-        return os.path.join(self.rootdir, f)
+#     def fullpath(self, f):
+#         return os.path.join(self.rootdir, f)
         
-    def __repr__(self):
-        return "<{0} at {1} top: {2} db: {3}>".format(
-            self.sysname, self.rootdir, self.topf, self.dbf)
+#     def __repr__(self):
+#         return "<{0} at {1} top: {2} db: {3}>".format(
+#             self.sysname, self.rootdir, self.topf, self.dbf)
--- run.py
+++ run.py
@@ -1,30 +1,14 @@
 #! /usr/bin/env python
 
-import os
+# import os
 import time
 import threading
 
-import yaml
-
-from obj import Master, Slave, System
-
-# get configuration from YAML env
-try:
-    yamlf = os.environ("YAML")
-except KeyError:
-    print "YAML env not found, serious problem!"
-
-params = yaml.load(open(yamlf))
-sysname = params['sysname']
-rootdir = params['rootdir']
-misc_params = params['MISC']
-system = System(sysname, rootdir, misc_params)
+from obj import Master, Slave
 
 ##################################MASTER PART##################################
+# on the master side, hostfile is for broadcasting url
 master = Master()
-
-# say_imyoung should be the first thing to do
-master.say_imyoung(system.db)  
 
 if master.big_master_flag:
     master.broadcast_url()
@@ -34,17 +18,21 @@ else:
 thread = threading.Thread(target=master.wakeup)
 
 ##################################SLAVE PART###################################
+# on the slave side, hostfile is for contacting
 slave = Slave()
-hostfile="__HOSTNAME__.txt"
+
+# say_imyoung should be the first thing to do
+slave.say_imyoung()
 
 while True:
-    cmd = slave.get_cmd(hostfile)
+    cmd = slave.get_cmd()
     slave.run(cmd)
     slave.report(cmd)
+
     # if less than 30 min is left, too old
     if slave.estimated_end_time - time.time() < (30 * 60):
         if slave.big_master_flag:
-            r = slave.request_switch(hostfile)
+            r = slave.request_switch()
             if r == "Done":                # meaning the swithing has succeeded
                 break
         else:
